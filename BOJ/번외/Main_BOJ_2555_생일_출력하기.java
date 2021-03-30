2554a31d022dc2fd9f76608fe9a12a2211e35b9d
0JUUU <61124319+0JUUU@users.noreply.github.com>
2021-03-30 23:32:02 +0900

    solve : boj 2555 생일 출력하기

---
+++ BOJ/번외/Main_BOJ_2555_생일_출력하기.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.io.*;
+
+public class Main_BOJ_2555_생일_출력하기 {
+	public static void main(String[] args) throws Exception {
+	       System.out.println("10/14");
+	}
+}
